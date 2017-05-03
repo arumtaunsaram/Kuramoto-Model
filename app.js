836afe24658d2d1f70a9e6fbcd14e8bdd18222e1
arumtaunsaram <arumtaunsaram@gmail.com>
2017-05-03 15:26:13 +0900

    Add Oscillator object.

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
-function turnGreen() {
-  document.getElementById("header").style.backgroundColor="#00ff00";
-}
+(function() {
+    var Oscillator = function () {
+        var _ = {};
+        /** @type Array */
+        _.coupled = [];
+        _.omega = 0;
+        _.lastTheta = 0;
+
+        _.addCoupledOscillator = function (oscilattor) {
+            this.coupled.push(oscilattor);
+        };
+
+        _.step = function () {
+          // Value between 0.0 and 1.0 will be stored
+          this.omega = Math.random();
+
+          this.lastTheta = this.omega; // TODO: Cast the calculated
+        };
+
+        return _;
+    };
+
+
+    window.App = !(typeof window['App'] !== 'undefined') ? (function () {
+        return {
+            init: function () {
+                var oscillators = [];
+                for (var i = 0; i < 2; i++) {
+                    oscillators.push(new Oscillator());
+                }
+
+                // Add 1 step to all oscillators
+                for (i = 0; i < oscillators.length; i++) {
+                  oscillators[i].step();
+                }
+
+                // Shows last thetas(debug)
+                for (i = 0; i < oscillators.length; i++) {
+                    console.log("Osc #" + i + ":" + oscillators[i].lastTheta);
+                }
+
+            }
+        };
+    })() : window.app;
+
+})();
