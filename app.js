23ab4b4ee6e4f694d040914805cf68aec56672a1
arumtaunsaram <arumtaunsaram@gmail.com>
2017-05-07 20:46:54 +0900

    Implement the 2nd term and coupling.

--- app.js
+++ app.js
@@ -6,15 +6,29 @@
         _.omega = 0;
         _.lastTheta = 0;
 
+        // Coefficient of 2nd term (K in Wikipedia)
+        _.coeff = 1.0;
+
         _.addCoupledOscillator = function (oscilattor) {
             this.coupled.push(oscilattor);
         };
 
         _.step = function () {
-          // Value between 0.0 and 1.0 will be stored
-          this.omega = Math.random();
+            // 1st term
+            // Value between 0.0 and 1.0 will be stored
+            this.omega = Math.random();
 
-          this.lastTheta = this.omega; // TODO: Cast the calculated
+            // Calculates the 2nd term
+            if (this.coupled.length < 1) {
+                this.lastTheta = this.omega;
+                return;
+            }
+
+            var sum = 0.0;
+            for (var i = 0; i < this.coupled.length; i++) {
+                sum += Math.sin(this.lastTheta - this.coupled[ i ].lastTheta);
+            }
+            this.lastTheta = this.omega - ((this.coeff / this.coupled.length) * sum);
         };
 
         return _;
@@ -26,18 +40,32 @@
             init: function () {
                 var oscillators = [];
                 for (var i = 0; i < 2; i++) {
+                    // Constructs oscillators.
                     oscillators.push(new Oscillator());
                 }
 
-                // Add 1 step to all oscillators
-                for (i = 0; i < oscillators.length; i++) {
-                  oscillators[i].step();
+                // Sets coupled oscillators
+                for (var target = 0; target < oscillators.length; target++) {
+                    for (i = 0; i < oscillators.length; i++) {
+                        if (i === target) {
+                            // Skips if the setting target and the oscillator to be set are same.
+                            continue;
+                        }
+                        oscillators[target].addCoupledOscillator(oscillators[i]);
+                    }
                 }
 
-                // Shows last thetas(debug)
-                for (i = 0; i < oscillators.length; i++) {
-                    console.log("Osc #" + i + ":" + oscillators[i].lastTheta);
-                }
+                window.setInterval(function() {
+                    // Add 1 step to all oscillators
+                    for (var i = 0; i < oscillators.length; i++) {
+                      oscillators[i].step();
+                    }
+
+                    // Shows last thetas(debug)
+                    for (i = 0; i < oscillators.length; i++) {
+                        console.log("Osc #" + i + ":" + oscillators[i].lastTheta);
+                    }
+                }, 10);
 
             }
         };
