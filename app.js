933a3c6d1845623ad147136f0c7cfa90a186d394
Yuki Kumazawa <arumtaunsaram at gmail dot com>
2017-05-26 16:57:41 +0900

    Add d3(v4) graph but data is not shown yet.

--- app.js
+++ app.js
@@ -35,13 +35,97 @@
     };
 
 
+    /**
+     * This class uses d3.js
+     * @constructor
+     */
+    function LineGraph() {
+        // Specifies the parent element this graph add to.
+       this.container = d3.select("#debugLineGraphs");
+       this.width = 960;
+       this.height = 200;
+       var margin = {};
+       margin.top = margin.right = margin.bottom = margin.left = 30;
+       this.margin = margin;
+       this.data = [];
+       this.x = d3.scaleLinear().range([0, this.width]);
+    }
+
+    LineGraph.prototype.render = function() {
+        if (!this.svg) { // render first time
+            this.y = d3.scaleLinear()
+                .range([this.height, 0]);
+
+            this.xAxis = d3.axisBottom()
+                .scale(this.x);
+            this.yAxis = d3.axisLeft()
+                .scale(this.y);
+
+            this.line = d3.line()
+                .x(function(d) { return x(d.x); })
+                .y(function(d) { return y(d.y); });
+                // Really need this?
+                //.interpolate("basis");
+            this.svg = this.container.append("svg")
+                .attr("width", this.width + this.margin.left + this.margin.right)
+                .attr("height", this.height + this.margin.top + this.margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+            //this.x.domain(d3.extent(data, function(d) { return d.x; }));
+            //this.y.domain(d3.extent(data, function(d) { return d.y; }));
+
+            this.svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + this.height + ")")
+                .call(this.xAxis);
+
+            this.svg.append("g")
+                .attr("class", "y axis")
+                .call(this.yAxis);
+
+            this.svg.append("path")
+                .datum(this.data)
+                .attr("stroke", "red")
+                .attr("d", this.line);
+        } else {
+            // Resets the target domain
+            this.x.domain(d3.extent(this.data, function(d) { return d.x; }));
+            this.y.domain(d3.extent(this.data, function(d) { return d.y; }));
+
+            this.svg.select("g.y")
+                .transition()
+                .duration(100)
+                .call(this.yAxis);
+
+            this.svg.select("g.x")
+                .transition()
+                .duration(100)
+                .call(this.xAxis);
+
+            this.svg.selectAll("path.line")
+                .datum(this.data)
+                .transition(100)
+                .attr("d", this.line);
+        }
+    };
+
+    LineGraph.prototype.addValue = function(value) {
+        this.data.push(value);
+    };
+
+
     window.App = !(typeof window['App'] !== 'undefined') ? (function () {
         return {
             init: function () {
                 var oscillators = [];
+                /** @type Array<LineGraph> */
+                var debugGraphs = [];
+
                 for (var i = 0; i < 2; i++) {
                     // Constructs oscillators.
                     oscillators.push(new Oscillator());
+                    debugGraphs.push(new LineGraph());
                 }
 
                 // Sets coupled oscillators
@@ -63,9 +147,11 @@
 
                     // Shows last thetas(debug)
                     for (i = 0; i < oscillators.length; i++) {
-                        console.log("Osc #" + i + ":" + oscillators[i].lastTheta);
+                        console.log("Osc #" + i + ":" + oscillators[ i ].lastTheta);
+                        debugGraphs[ i ].addValue(oscillators[ i ].lastTheta);
+                        debugGraphs[ i ].render();
                     }
-                }, 10);
+                }, 300);
 
             }
         };
